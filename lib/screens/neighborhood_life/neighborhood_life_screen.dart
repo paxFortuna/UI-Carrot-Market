f9c6c7b14250479cffadca66943959cc11b56228
fortuna <avidia38@gmail.com>
2022-11-01 09:41:29 +0900

    feat: neighborhood_life screen

--- lib/screens/neighborhood_life/neighborhood_life_screen.dart
+++ lib/screens/neighborhood_life/neighborhood_life_screen.dart
@@ -1,4 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../../models/neighborhood_life.dart';
+import '../components/appbar_preferred_size.dart';
+import 'components/life_body.dart';
+import 'components/life_header.dart';
 
 class NeighborhoodLifeScreen extends StatelessWidget {
   const NeighborhoodLifeScreen({Key? key}) : super(key: key);
@@ -6,13 +12,52 @@ class NeighborhoodLifeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[100],
       appBar: AppBar(
-        title: const Text('Neighbor',
+        title: const Text(
+          'Neighbor',
           style: TextStyle(
             color: Colors.black,
-          ),),
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              CupertinoIcons.search,
+              color: Colors.deepPurple,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+              icon: const Icon(
+                CupertinoIcons.plus_rectangle_on_rectangle,
+                color: Colors.deepPurple,
+              ),
+              onPressed: () {}),
+          IconButton(
+            icon: const Icon(
+              CupertinoIcons.bell,
+              color: Colors.deepPurple,
+            ),
+            onPressed: () {},
+          ),
+        ],
+        bottom: appBarBottomLine,
       ),
-      body: Container(),
+      body: ListView(
+        children: [
+          LifeHeader(),
+          ...List.generate(
+            neighborhoodLifeList.length,
+            (index) => Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: LifeBody(
+                neighborhoodLife: neighborhoodLifeList[index],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
